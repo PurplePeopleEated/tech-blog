b0ff5b5bae3f2fd0137b1b8b2de48dedcf05cdad
Karra <karra.maynard@gmail.com>
2024-02-04 16:55:51 -0500

    push to heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const sequelize = require('./config/connection');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
