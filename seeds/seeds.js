97d2d173ede5ad6e05f2ce79d2eebd3ef5df14b0
Karra <karra.maynard@gmail.com>
2024-02-04 19:35:08 -0500

    add logic to create both multiple posts and multiple comments

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -14,6 +14,20 @@ const seedDatabase = async () => {
     returning: true
   });
 
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id
+    });
+  }
+
   process.exit(0);
 };
 
