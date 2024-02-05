865eee700bd91c9e656aeca3f19f3cca742522f0
Karra <karra.maynard@gmail.com>
2024-02-04 20:08:57 -0500

    setup handlebars engine

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const sequelize = require('./config/connection');
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
-const PORT = process.env.PORT || 3001;
+const utils = require('./helpers/utils');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine
+const hbs = exphbs.create({utils});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
