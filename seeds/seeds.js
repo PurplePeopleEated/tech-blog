c3f4c9bdb66ea76230846abbf09dc36cbf1587e5
Karra <karra.maynard@gmail.com>
2024-02-04 19:34:03 -0500

    create seedDatabase function

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -5,3 +5,16 @@ const { Post, User, Comment } = require('../models');
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
