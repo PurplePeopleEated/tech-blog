27394d037aa53f3bfb7541ecf6f53e5e710287f3
Karra <karra.maynard@gmail.com>
2024-02-04 21:40:28 -0500

    add comments and create cookie

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ const utils = require('./helpers/utils');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Initialize Express.js
 const app = express();
+// Set up port
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine
+
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({utils});
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Turn on routes
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
