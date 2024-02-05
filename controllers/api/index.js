05e00cc1ce2462711437267f23fd93b5545ebb89
Karra <karra.maynard@gmail.com>
2024-02-04 20:07:46 -0500

    create structure for routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const userRoutes = require('./user');
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use('/', userRoutes);
 
 module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+} catch (err) {
+    res.status(500).json(err);
+  }
+});
