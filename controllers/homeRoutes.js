c5f330fe87e4d99ab4b5e56c8695cd7df0c9c4c5
Karra <karra.maynard@gmail.com>
2024-02-09 15:42:38 -0500

    GET route for login/signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+// * GET login page * //
+router.get('/login', async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
