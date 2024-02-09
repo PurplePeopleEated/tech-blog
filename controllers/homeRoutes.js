7ba9ad96db26c2070b5c7164cb2b6b9426706151
Karra <karra.maynard@gmail.com>
2024-02-09 15:43:21 -0500

    GET route for profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,4 +37,23 @@ router.get('/login', async (req, res) => {
   res.render('login');
 });
 
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }]
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
